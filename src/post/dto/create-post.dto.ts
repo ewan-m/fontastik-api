7ac20b84c1e67c02d392ed6d70297f9f2a2951d8
Ewan Morrison <ewanmorrrison@gmail.com>
2020-09-04 10:23:45 +0100

    Add validation to create post dto

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,15 +1,27 @@
-import { IsString, IsNotEmpty, IsNumber } from "class-validator";
+import {
+	IsString,
+	IsNotEmpty,
+	IsNumber,
+	MaxLength,
+	Max,
+	Min,
+} from "class-validator";
 
 export class CreatePostDto {
 	@IsNotEmpty()
 	@IsString()
+	@MaxLength(420)
 	content: string;
 
 	@IsNotEmpty()
-	@IsNumber()
+	@IsNumber({ maxDecimalPlaces: 2 })
+	@Max(90)
+	@Min(-90)
 	latitude: number;
 
 	@IsNotEmpty()
-	@IsNumber()
+	@IsNumber({ maxDecimalPlaces: 2 })
+	@Max(180)
+	@Min(-180)
 	longitude: number;
 }
