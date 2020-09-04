db74e24f5fd8258e4832cc861e3db143e9af00e9
Ewan Morrison <ewanmorrrison@gmail.com>
2020-09-04 10:19:58 +0100

    Add swagger ui in dev

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,39 @@
 import { config } from "dotenv";
 config();
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe, INestApplication } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { NestFactory } from "@nestjs/core";
 import * as bodyParser from "body-parser";
 import { AppModule } from "./app.module";
 import { useContainer } from "class-validator";
 import { AddRefreshTokenOnExpiryInterceptor } from "./interceptors/add-refresh-token-on-expiry.interceptor";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
+function enableSwagger(app: INestApplication): void {
+	if (process.env.ENABLE_SWAGGER === "true") {
+		const options = new DocumentBuilder()
+			.setTitle("Fontastik API")
+			.setDescription("The fontastik API endpoints")
+			.setVersion("1.0")
+			.addTag("fontastik")
+			.build();
+		const document = SwaggerModule.createDocument(app, options);
+		SwaggerModule.setup("swagger", app, document);
+	}
+}
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
 		cors: { origin: process.env.APPLICATION_URL },
 	});
+	app.disable("x-powered-by");
 	app.useGlobalPipes(new ValidationPipe());
 	app.use(bodyParser.json({ limit: "50mb" }));
 	app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 	app.useGlobalInterceptors(new AddRefreshTokenOnExpiryInterceptor());
 	useContainer(app.select(AppModule), { fallbackOnErrors: true });
+	enableSwagger(app);
+
 	await app.listen(3000);
 }
 bootstrap();
