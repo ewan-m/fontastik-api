f003e3b939bb2f33fbc6c86991036ce6c7f4d5f5
Ewan Morrison <ewanmorrrison@gmail.com>
2020-09-04 10:21:34 +0100

    Abstract token parsing for user id to service

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { FontRepository } from "./font/db/font.repository";
 import { DatabaseModule } from "./db/database.module";
 import { UserRepository } from "./auth/db/user.repository";
 import { GithubService } from "./services/github.service";
+import { TokenParserService } from "./services/token-parser.service";
 
 @Module({
 	imports: [
@@ -30,6 +31,7 @@ import { GithubService } from "./services/github.service";
 		FontRepository,
 		UserRepository,
 		GithubService,
+		TokenParserService,
 	],
 })
 export class AppModule {}
---
+++ src/services/token-parser.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, BadRequestException } from "@nestjs/common";
+import { TokenPayload } from "src/auth/token-payload.type";
+import { decode } from "jsonwebtoken";
+
+@Injectable()
+export class TokenParserService {
+	public getUserId(authHeader: string) {
+		const token = authHeader.split(" ")?.[1];
+
+		if (token) {
+			const userId = (decode(token) as TokenPayload).id;
+
+			if (userId) {
+				return userId;
+			}
+		}
+
+		throw new BadRequestException([
+			"There is something wrong with your session. Please try logging out and in again.",
+		]);
+	}
+}
