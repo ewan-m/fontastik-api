c9a9e15f8475a69976f04a99a984e626cce9ad9a
Ewan Morrison <ewanmorrrison@gmail.com>
2020-09-25 09:03:30 +0100

    Tidy up queries in the user repository

--- src/auth/db/user.repository.ts
+++ src/auth/db/user.repository.ts
@@ -10,11 +10,9 @@ export class UserRepository {
 	public async getUserByEmail(email: string): Promise<User | undefined> {
 		return (
 			await this.db.query<User>(
-				`
-SELECT user_id, email, name, password_hash, password_salt, is_blocked
+				`SELECT user_id, email, name, password_hash, password_salt, is_blocked
 FROM user_identity
-WHERE email = $1;
-		`,
+WHERE email = $1;`,
 				[email]
 			)
 		).rows[0];
@@ -23,11 +21,9 @@ WHERE email = $1;
 	public async updatePassword(user: User) {
 		const { user_id, password_hash, password_salt } = user;
 		return await this.db.query(
-			`
-UPDATE user_identity
+			`UPDATE user_identity
 SET password_hash = $1, password_salt = $2
-WHERE user_id = $3;
-`,
+WHERE user_id = $3;`,
 			[password_hash, password_salt, user_id]
 		);
 	}
@@ -35,8 +31,7 @@ WHERE user_id = $3;
 	public async createUser(user: User) {
 		const { name, email, password_hash, password_salt } = user;
 		return await this.db.query(
-			`
-INSERT INTO user_identity (name, email, password_hash, password_salt)
+			`INSERT INTO user_identity (name, email, password_hash, password_salt)
 VALUES ($1, $2, $3, $4)
 RETURNING user_id;`,
 			[name, email, password_hash, password_salt]
