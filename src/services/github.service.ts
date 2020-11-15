ea16dff37ff255332eefea93dfd820bf8a7e9468
Ewan Morrison <ewanmorrrison@gmail.com>
2020-11-15 17:11:45 +0000

    Allow github saved file to be overwritten

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -5,6 +5,7 @@ export class GithubService {
 	private githubAccessToken: string;
 	private readonly githubUrl =
 		"https://api.github.com/repos/ewan-m/fontastik-data/contents";
+	private readonly acceptHeader = "application/vnd.github.v3+json";
 	private githubBranch: string;
 
 	constructor(private http: HttpService) {
@@ -12,7 +13,30 @@ export class GithubService {
 		this.githubBranch = process.env.GITHUB_BRANCH;
 	}
 
-	postFile(fileName: string, content: string, commitMessage: string) {
+	async postFile(fileName: string, content: string, commitMessage: string) {
+		try {
+			const file = await this.http
+				.get(`${this.githubUrl}/${fileName}?ref=${this.githubBranch}`, {
+					headers: {
+						accept: this.acceptHeader,
+						authorization: `token ${this.githubAccessToken}`,
+					},
+				})
+				.toPromise();
+			if (file.data.sha) {
+				await this.http
+					.delete(
+						`${this.githubUrl}/${fileName}?branch=${this.githubBranch}&sha=${file.data.sha}&message=Revert: '${commitMessage}'`,
+						{
+							headers: {
+								accept: this.acceptHeader,
+								authorization: `token ${this.githubAccessToken}`,
+							},
+						}
+					)
+					.toPromise();
+			}
+		} catch (error) {}
 		return this.http
 			.put(
 				`${this.githubUrl}/${fileName}`,
@@ -23,7 +47,7 @@ export class GithubService {
 				},
 				{
 					headers: {
-						accept: "application/vnd.github.v3+json",
+						accept: this.acceptHeader,
 						authorization: `token ${this.githubAccessToken}`,
 					},
 				}
